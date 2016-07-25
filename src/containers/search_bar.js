f195ab4f8db9d807536d0f8c378433b7a4d49385
dsomel21 <dsomel21@gmail.com>
2016-07-25 17:13:10 -0400

    Got searchbar working nicely, and made use of the bind function with the state

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
-	render() {
-		return {
-			<form className="input-group">
-				<input />
-			</form>
-		}
+	
+	constructor(props){
+		super(props);
+
+		this.state = { search_query: '' };
+		/* Kinda like overriding the method */
+		this.onInputChange = this.onInputChange.bind(this);
 	}
+
+	onInputChange(e) {
+		this.setState({
+			search_query: e.target.value
+		});
+	}
+
+	handleSearchAction(e){
+		console.log(e.target.value);	
+	}
+
+	render() {
+		return (
+			<form className="input-group">
+				<input 
+					placeholder="Toronto, ON"
+					className="form-control"
+					value={this.state.search_query}
+					// onChange={(e) => this.onInputChange(e)}	
+					onChange={this.onInputChange}	
+				/>
+				<button type="button" className="btn btn-secondary"
+					onClick={this.handleSearchAction}>Search</button>
+			</form>
+		)
+	}
+
 }
